8d8f72d468700b66f942890bd342c1059f9ea782
knowonehome <knowonehome@gmail.com>
2015-06-01 19:25:05 +0000

    fixed some bootstrap and deleted devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150522133508) do
 
   create_table "fireworks", force: true do |t|
@@ -21,6 +20,5 @@ ActiveRecord::Schema.define(version: 20150522133508) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
 
 end
