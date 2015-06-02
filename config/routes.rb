39afe6052b372b40cb3b98a5b375ef455be8dbb4
knowonehome <knowonehome@gmail.com>
2015-06-02 20:51:24 +0000

    updated views for Fireworks and root to welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :fireworks
 
   
-  root to: 'fireworks#index'
+  root to: 'welcome#index'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
