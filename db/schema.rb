0996347f66d02a52be920789c34c96ca024e576e
knowonehome <knowonehome@gmail.com>
2019-06-11 10:51:38 -0500

    installed bootstrap 4 for webpacker
    Added some examples

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_11_133932) do
+ActiveRecord::Schema.define(version: 2019_06_11_152849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "fireworks", force: :cascade do |t|
+    t.string "firework_name"
+    t.string "style"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
